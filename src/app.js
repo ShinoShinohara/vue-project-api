01928f71930a312810c69a9a83d74386e732b41a
shinoshinohara <vshinohara@outlook.com>
2022-09-26 07:13:42 -0400

    objection config

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const dotenv = require("dotenv");
+const Knex = require("knex");
+const { Model } = require("objection");
 
 dotenv.config();
 const app = express();
+
+// objection set up
+const knexfile = require("./knexfile");
+const knex = Knex(knexfile[process.env.NODE_ENV]);
+Model.knex(knex);
 
 app.use(bodyParser.json());
 
